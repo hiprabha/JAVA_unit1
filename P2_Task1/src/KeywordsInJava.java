815a84626f28101ca3938ca560671d7c0154f88c
hp <hp@LAPTOP-RH8KN3BE>
2022-04-03 18:38:01 +0530

    Keywords In Java

---
+++ P2_Task1/src/KeywordsInJava.java
@@ -0,0 +1,18 @@
+
+public class KeywordsInJava 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter two numbers");
+	    int static = 10;  //Keywords cannot be used as a variable name in Java.
+	    int second = 20;
+	    
+	    System.out.println(first + " " + second);
+
+	    // add two numbers
+	    int sum = first + second;
+	    System.out.println("The sum is: " + sum);
+	}
+
+}
