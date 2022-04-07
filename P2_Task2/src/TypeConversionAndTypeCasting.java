766e8a3d8582764bde9fd042c71a271f8b01ce27
hp <hp@LAPTOP-RH8KN3BE>
2022-04-07 16:25:30 +0530

    Type Conversion and Type Casting in Java

---
+++ P2_Task2/src/TypeConversionAndTypeCasting.java
@@ -0,0 +1,22 @@
+
+public class TypeConversionAndTypeCasting 
+{
+
+	public static void main(String[] args) 
+	{
+		double d ;
+		int i = 23;
+		
+		d = i;            //Type Conversion
+		
+		double e = 64;
+		int j;
+		
+		j = (int)e;      //Type Casting
+		
+		System.out.println(i);
+		System.out.println(j);
+
+	}
+
+}
