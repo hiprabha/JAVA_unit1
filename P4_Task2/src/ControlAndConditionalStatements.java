60cad6c3303df66d010814c4b32e6de8cf7a9ab1
hp <hp@LAPTOP-RH8KN3BE>
2022-04-08 19:03:32 +0530

    Control and Conditional Statements in Java

---
+++ P4_Task2/src/ControlAndConditionalStatements.java
@@ -0,0 +1,51 @@
+
+public class ControlAndConditionalStatements 
+{
+
+	public static void main(String[] args) 
+	{
+		int i=1;                       //Java do-while Loop
+	    do{    
+	        System.out.println(i);    
+	    i++;    
+	    }while(i<=10);    
+	    
+	    
+	    for(int j=1;j<=10;j++)        //Java Continue Statement
+	    {  
+	        if(j==5)
+	        {   
+	            continue; 
+	        }  
+	        System.out.println(j);  
+	    }  
+	    
+	    
+	    int k=1;                      //Java While Loop
+	    while(k<=10)
+	    {  
+	        System.out.println(k);  
+	    k++;  
+	    }  
+	    
+	    
+	    int number=13;  
+	                                  
+	    if(number%2==0)              //Java If-else Statement
+	    {                             
+	        System.out.println("even number");  
+	    }
+	    else
+	    {  
+	        System.out.println("odd number");  
+	    }  
+	    
+	    
+	    for(int l=1;l<=10;l++)        //Loop in Java
+	    {  
+	        System.out.println(l);  
+	    }  
+	   
+	}
+
+}
