a598ec9f7d64375135c1f588fa4476a6d19dfcba
hp <hp@LAPTOP-RH8KN3BE>
2022-04-08 21:32:58 +0530

    Class and Object in Java

---
+++ P6_Task1/src/ClassAndObject.java
@@ -0,0 +1,18 @@
+
+public class ClassAndObject 
+{
+
+	int x = 5;
+	int y = 10;
+	
+	public static void main(String[] args) 
+	{
+		ClassAndObject myObj1= new ClassAndObject();
+		ClassAndObject myObj2= new ClassAndObject();
+		
+		System.out.println(myObj1.x);
+		System.out.println(myObj2.y);
+
+	}
+
+}
