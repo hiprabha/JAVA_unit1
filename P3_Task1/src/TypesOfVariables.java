2677cdaeedf5879a7726581ba6aa405d014d62d8
hp <hp@LAPTOP-RH8KN3BE>
2022-04-08 18:29:29 +0530

    Types Of Variables in Java

---
+++ P3_Task1/src/TypesOfVariables.java
@@ -0,0 +1,24 @@
+
+public class TypesOfVariables 
+{
+	
+	static int m = 20;//static variable  
+	
+	
+	int n = 30;      //instance variable 
+	
+	
+	void method()  
+    {    
+        int a = 50;//local variable  
+        System.out.println("THIS IS LOCAL VARIABLE");
+        System.out.println(a);
+    }  
+	
+
+	public static void main(String[] args) 
+	{   
+		System.out.println("TYPES OF VARIABLES IN Java");
+	}
+
+}
